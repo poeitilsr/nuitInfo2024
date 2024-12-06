909db17e4786bf996185e6723d41be114f53b767
Poeiti LESUEUR <poeiti.lesueur@epitech.eu>
2024-12-06 01:24:24 +0100

    ok

---
+++ 123script.js
@@ -0,0 +1,6 @@
+// Set background image dynamically
+document.getElementById('backgroundImage').src = 'Illustration_sans_titre_3.png';
+
+// Set overlay image dynamically
+document.getElementById('overlayImage').src = 'IMG_0087-removebg-preview_avec_arriere-plan_supprime.png';
+
