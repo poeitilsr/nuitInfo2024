fa61501c646fd3e1b9986c9d4fe64b88acaabb62
Poeiti LESUEUR <poeiti.lesueur@epitech.eu>
2024-12-06 02:40:18 +0100

    new organs

--- 123script.js
+++ 123script.js
@@ -5,3 +5,28 @@ const overlayImage = document.getElementById('overlayImage');
 overlayImage.addEventListener('click', () => {
   overlayImage.classList.toggle('zoomed'); // Toggle the zoom class
 });
+
+// Get the overlay image element
+const overlayImage2 = document.getElementById('overlayImage2');
+
+// Add a click event listener to toggle the zoom effect
+overlayImage2.addEventListener('click', () => {
+  overlayImage2.classList.toggle('zoomed'); // Toggle the zoom class
+});
+
+// Get the overlay image element
+const overlayImage3 = document.getElementById('overlayImage3');
+
+// Add a click event listener to toggle the zoom effect
+overlayImage3.addEventListener('click', () => {
+  overlayImage3.classList.toggle('zoomed'); // Toggle the zoom class
+});
+
+
+// Get the overlay image element
+const overlayImage4 = document.getElementById('overlayImage4');
+
+// Add a click event listener to toggle the zoom effect
+overlayImage4.addEventListener('click', () => {
+  overlayImage4.classList.toggle('zoomed'); // Toggle the zoom class
+});
